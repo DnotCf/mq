bbe3dca3b62c80d055219db7aa0e12edda2d06d0
tangs <139841644@qq.com>
2021-08-20 17:20:18 +0800

    预留check

--- site-starter/src/main/resources/sql/srig.sql
+++ site-starter/src/main/resources/sql/srig.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80025
 File Encoding         : 65001
 
-Date: 2021-08-18 18:24:35
+Date: 2021-08-20 17:19:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -33,7 +33,9 @@ CREATE TABLE `SRIG_FORWARD_ROUTER` (
 -- ----------------------------
 -- Records of SRIG_FORWARD_ROUTER
 -- ----------------------------
-INSERT INTO `SRIG_FORWARD_ROUTER` VALUES ('71b3f62679a44567bed31162ee3df096', '586a12c1284a466e87492936a10e2930', 'a4abc334d90b4be1b1a975dde16d3dd7', 'dws_hw_gps_devc_tp_gpspoint_rt', 'test', '2021-08-26 10:17:15', '1');
+INSERT INTO `SRIG_FORWARD_ROUTER` VALUES ('71b3f62679a44567bed', 'a4abc334d90b4be1b1a975dde16d3dd7', '918a836419f04c4e80be437a54cc03d9', 'dws_hw_gps_devc_tp_gpspoint_rt', 'aly_test', '2021-08-18 09:07:36', '0');
+INSERT INTO `SRIG_FORWARD_ROUTER` VALUES ('71b3f62679a44567bed31162e', 'a4abc334d90b4be1b1a975dde16d3dd7', '586a12c1284a466e87492936a10e2930', 'dws_hw_gps_devc_tp_gpspoint_rt', 'aly_test', '2021-08-21 09:07:36', '0');
+INSERT INTO `SRIG_FORWARD_ROUTER` VALUES ('71b3f62679a44567bed31162ee3df096', 'a4abc334d90b4be1b1a975dde16d3dd7', '0d29b50e718e4b16971065747e4099f7', 'dws_hw_gps_devc_tp_gpspoint_rt', 'aly_test', '2021-08-19 09:07:36', '0');
 
 -- ----------------------------
 -- Table structure for SRIG_MQ_SERVER
@@ -41,6 +43,7 @@ INSERT INTO `SRIG_FORWARD_ROUTER` VALUES ('71b3f62679a44567bed31162ee3df096', '5
 DROP TABLE IF EXISTS `SRIG_MQ_SERVER`;
 CREATE TABLE `SRIG_MQ_SERVER` (
   `ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `NAME` varchar(100) DEFAULT NULL COMMENT '数据源名称',
   `IP` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `PORT` int DEFAULT NULL,
   `PROTOCOL` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
@@ -63,17 +66,17 @@ CREATE TABLE `SRIG_MQ_SERVER` (
   `VPN_ACCONT` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT 'vpn账号',
   `VPN_PASSWORD` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT 'vpn密码',
   `DEL_FLAG` tinyint(1) DEFAULT '0' COMMENT '0未删除1删除',
+  `SOURCE_TYPE` tinyint(1) DEFAULT '0' COMMENT '0源数据1目标数据',
   PRIMARY KEY (`ID`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of SRIG_MQ_SERVER
 -- ----------------------------
-INSERT INTO `SRIG_MQ_SERVER` VALUES ('356815f42d4c45719c59a34921c9d4fc', 'localhost', '1883', 'tcp', '/', 'MQTT', 'admin', 'public', null, null, '2021-08-18 17:55:23', '2021-08-18 17:55:23', null, null, 'GID_TRACE_FUNSION_TT', null, '*', 'xHbzq7ntbOkS7HTPK2HqGBDX93UDkj', 'LTAI4GDmh8jPmnnozLrcNyLY', null, null, null, '0');
-INSERT INTO `SRIG_MQ_SERVER` VALUES ('586a12c1284a466e87492936a10e2930', 'localhost', '9876', 'tcp', 'target', 'Aliyun_RocketMQ', '', '', null, null, '2021-08-18 17:53:05', '2021-08-18 17:53:05', 'http://MQ_INST_1551934212811443_BXdv0RAB.mq-internet-access.mq-internet.aliyuncs.com:80', null, 'GID_TRACE_FUNSION_TT', 'dws_hw_gps_devc_tp_gpspoint_rt', '*', 'xHbzq7ntbOkS7HTPK2HqGBDX93UDkj', 'LTAI4GDmh8jPmnnozLrcNyLY', null, null, null, '0');
-INSERT INTO `SRIG_MQ_SERVER` VALUES ('a4abc334d90b4be1b1a975dde16d3dd7', 'localhost', '5672', 'tcp', '/', 'AMQP', 'guest', 'guest', null, null, '2021-08-18 18:11:22', '2021-08-18 18:11:22', null, null, 'GID_TRACE_FUNSION_TT', null, '*', 'xHbzq7ntbOkS7HTPK2HqGBDX93UDkj', 'LTAI4GDmh8jPmnnozLrcNyLY', null, null, null, '0');
-INSERT INTO `SRIG_MQ_SERVER` VALUES ('c1902fad2ae54886b9c5c5e4ffc29d47', 'localhost', '9876', 'tcp', 'target', 'RocketMQ', '', '', null, null, '2021-08-18 17:54:19', '2021-08-18 17:54:19', 'localhost:9876', null, 'GID_TRACE_FUNSION_TT', 'test', '*', 'xHbzq7ntbOkS7HTPK2HqGBDX93UDkj', 'LTAI4GDmh8jPmnnozLrcNyLY', null, null, null, '0');
-INSERT INTO `SRIG_MQ_SERVER` VALUES ('c255c7a2aaeb404784f264a028bac90b', 'localhost', '5672', 'tcp', '/', 'AMQP', 'guest', 'guest', null, null, '2021-08-18 17:56:14', '2021-08-18 17:56:14', null, null, 'GID_TRACE_FUNSION_TT', null, '*', 'xHbzq7ntbOkS7HTPK2HqGBDX93UDkj', 'LTAI4GDmh8jPmnnozLrcNyLY', null, null, null, '0');
+INSERT INTO `SRIG_MQ_SERVER` VALUES ('0d29b50e718e4b16971065747e4099f7', 'RocketMQ服务', 'localhost', '9876', 'tcp', '/', 'RocketMQ', 'admin', 'public', null, null, '2021-08-20 10:47:42', '2021-08-20 10:47:42', 'localhost:9876', null, 'GID_TRACE_FUNSION_TT', 'dws_hw_gps_devc_tp_gpspoint_rt', '*', 'xHbzq7ntbOkS7HTPK2HqGBDX93UDkj', 'LTAI4GDmh8jPmnnozLrcNyLY', '1', 'test', 'test', '0', '1');
+INSERT INTO `SRIG_MQ_SERVER` VALUES ('586a12c1284a466e87492936a10e2930', 'Rabbit服务', 'localhost', '5672', 'tcp', '/', 'AMQP', 'guest', 'guest', null, null, '2021-08-18 17:53:05', '2021-08-20 10:44:18', null, null, 'GID_TRACE_FUNSION_TT', 'dws_hw_gps_devc_tp_gpspoint_rt', '*', 'xHbzq7ntbOkS7HTPK2HqGBDX93UDkj', 'LTAI4GDmh8jPmnnozLrcNyLY', '1', 'test', 'test', '0', '1');
+INSERT INTO `SRIG_MQ_SERVER` VALUES ('918a836419f04c4e80be437a54cc03d9', 'MQTT数据源名称', 'localhost', '1883', 'tcp', '/clzy', 'MQTT', 'admin', 'public', null, null, '2021-08-20 10:45:14', '2021-08-20 11:13:27', null, null, 'GID_TRACE_FUNSION_TT', 'dws_hw_gps_devc_tp_gpspoint_rt', '*', 'xHbzq7ntbOkS7HTPK2HqGBDX93UDkj', 'LTAI4GDmh8jPmnnozLrcNyLY', '1', 'test', 'test', '0', '1');
+INSERT INTO `SRIG_MQ_SERVER` VALUES ('a4abc334d90b4be1b1a975dde16d3dd7', '阿里云RocketMQ', 'localhost', '5672', 'tcp', '/', 'Aliyun_RocketMQ', 'guest', 'guest', null, null, '2021-08-18 18:11:22', '2021-08-20 16:43:14', 'http://MQ_INST_1551934212811443_BXdv0RAB.mq-internet-access.mq-internet.aliyuncs.com:80', null, 'GID_TRACE_FUNSION_TT', 'dws_hw_gps_devc_tp_gpspoint_rt', '*', 'xHbzq7ntbOkS7HTPK2HqGBDX93UDkj', 'LTAI4GDmh8jPmnnozLrcNyLY', '1', 'test', 'test', '0', '0');
 
 -- ----------------------------
 -- Table structure for SRIG_RSU
