3e63d36703df9475b7de80edf38e0bf5985b8b6b
tangs <139841644@qq.com>
2021-08-25 18:23:56 +0800

    kafka多线程优化

--- site-starter/src/main/java/com/clzy/srig/mq/integration/controller/SrigForwardRouterController.java
+++ site-starter/src/main/java/com/clzy/srig/mq/integration/controller/SrigForwardRouterController.java
@@ -150,6 +150,9 @@ public class SrigForwardRouterController extends BaseController {
     @GetMapping("start")
     public JsonResponse startConnection(String id) {
         ForwardRouter router = service.get(id);
+        if (router.getExpireTime() != null && router.getExpireTime().compareTo(new Date()) < 0) {
+            return JsonResponse.error(-1, "请延长过期时间再试", "请延长过期时间");
+        }
         forwardService.addRouterTable(router);
         router.setExpireTime(null);
         router.setStatus(MQStuats.online.getCode());
